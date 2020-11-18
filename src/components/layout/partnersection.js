84eca3930ef24fc4e9e766d3b0ed53614abf7a25
rashonmitchell <rashonmitchell313@gmail.com>
2020-11-17 19:47:39 -0600

    updated svg for partner resource

--- src/components/layout/partnersection.js
+++ src/components/layout/partnersection.js
@@ -1,71 +1,114 @@
 import React from 'react';
 
 function PartnerSection(){
+    const svgStyle = {
+        textAnchor: "start",
+        padding: "10px",
+        fontSize: "2.8em",
+        fontWeight:"500"
+      };
     return(
         <section className="pt-20 pb-8">
             <div className="max-w-screen-xl container mx-auto px-4">
                 <div className="-mx-5 flex flex-wrap">
                     <div className="w-full flex flex-col p-5 sm:w-1/2 lg:w-1/4">
-                        <div className="justify-center flex flex-col flex-none px-10 pt-10 pb-12 blue-delivers-square-img">
-                            <div className="flex-1">
-                                <h2 className="text-cta text-xl sm:text-xl lg:text-xl font-bold leading-snug">
-                                    Partner Program Overview
-                                </h2>
-                            </div>
-                            <a href="#" 
-                                className="mt-4 inline-flex items-center py-3 text-kddiOrange font-normal text-2xl">
-                                Download &#8594;
-                            </a>
-                        </div>
+                        <svg width="100%" height="100%" viewBox="0 0 599 299" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                            <g id="Page-1" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+                            <g id="Partner-Page" transform="translate(-336.000000, -1707.000000)" fill="#FFFFFF" stroke="#AED6F2" strokeWidth={8}>
+                                <g id="Modernize" transform="translate(340.000000, 1711.000000)">
+                                <polygon id="Rectangle-Copy-5" points="0 1.75873205e-13 591 0 591 217.603692 532.82783 291 0 291" />
+                                
+                                <text x="60" y="70" fontWeight="500" fill="#2B3175" stroke="none" fontSize="2.8em">
+                                    Partner Program 
+                                </text>
+                                <text x="60" y="130" fontWeight="500" fill="#2B3175" stroke="none" fontSize="2.8em">
+                                    Overview
+                                </text>
+                                <a href="https://www.kddiacloud.com/pdf/Accelerate-Your-Hybrid-and-Multi-Cloud-Strategy-with-KDDI-America-Telehouse.pdf">
+                                    <text x="60" y="250" fontFamily="Arial" fontWeight="normal" fill="#E86A24" stroke="none" fontSize="3.3em">
+                                        Download &#8594;
+                                    </text> 
+                                </a>
+                                </g>
+                            </g>
+                            </g>
+                        </svg>
                     </div>
-                
+
                     <div className="w-full flex flex-col p-5 sm:w-1/2 lg:w-1/4">
-                        <div className="justify-center flex flex-col flex-none px-10 pt-10 pb-12 blue-delivers-square-img">
-                            <div className="flex-1">
-                                <h2 className="text-cta text-xl sm:text-xl lg:text-xl font-bold leading-snug">
-                                    Cloud Services Brochure
-                                </h2>
-                            </div>
-                            <a href="https://www.kddiacloud.com/pdf/Accelerate-Your-Hybrid-and-Multi-Cloud-Strategy-with-KDDI-America-Telehouse.pdf" 
-                                className="mt-4 inline-flex items-center py-3 text-kddiOrange font-normal text-2xl">
-                                Download &#8594;
-                            </a>
-                        </div>
+                        <svg width="100%" height="100%" viewBox="0 0 599 299" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                            <g id="Page-1" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+                            <g id="Partner-Page" transform="translate(-336.000000, -1707.000000)" fill="#FFFFFF" stroke="#AED6F2" strokeWidth={8}>
+                                <g id="Modernize" transform="translate(340.000000, 1711.000000)">
+                                <polygon id="Rectangle-Copy-5" points="0 1.75873205e-13 591 0 591 217.603692 532.82783 291 0 291" />
+                                <text x="60" y="70" fontWeight="500" fill="#2B3175" stroke="none" fontSize="2.8em">
+                                    Cloud Services 
+                                </text>
+                                <text x="60" y="130" fontWeight="500" fill="#2B3175" stroke="none" fontSize="2.8em">
+                                    Brochure
+                                </text>
+                                <a href="https://www.kddiacloud.com/pdf/Accelerate-Your-Hybrid-and-Multi-Cloud-Strategy-with-KDDI-America-Telehouse.pdf">
+                                    <text x="60" y="250" fontFamily="Arial" fontWeight="normal" fill="#E86A24" stroke="none" fontSize="3.3em">
+                                        Download &#8594;
+                                    </text> 
+                                </a>
+                                </g>
+                            </g>
+                            </g>
+                        </svg>
                     </div>
-                
+
                     <div className="w-full flex flex-col p-5 sm:w-1/2 lg:w-1/4">
-                        <div className="flex flex-col flex-none px-10 pt-10 pb-12 blue-delivers-square-img">
-                            <div className="flex-1">
-                                <h2 className="text-cta text-xl sm:text-xl lg:text-xl font-bold leading-snug">
-                                    Cloud 
-                                    Services 
-                                    Video  
-                                </h2>
-                            </div>
-                            <a href="# https://youtu.be/5Jv9XXSEFdk" 
-                                className="mt-4 inline-flex items-center py-3 text-kddiOrange font-normal text-2xl">
-                                Watch &#8594;
-                            </a>
-                        </div>
+                        <svg width="100%" height="100%" viewBox="0 0 599 299" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                            <g id="Page-1" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+                            <g id="Partner-Page" transform="translate(-336.000000, -1707.000000)" fill="#FFFFFF" stroke="#AED6F2" strokeWidth={8}>
+                                <g id="Modernize" transform="translate(340.000000, 1711.000000)">
+                                <polygon id="Rectangle-Copy-5" points="0 1.75873205e-13 591 0 591 217.603692 532.82783 291 0 291" />
+                                <text x="60" y="70" fontWeight="500" fill="#2B3175" stroke="none" fontSize="2.8em">
+                                    Cloud Services 
+                                </text>
+                                <text x="60" y="130" fontWeight="500" fill="#2B3175" stroke="none" fontSize="2.8em">
+                                    Video 
+                                </text>
+                                <a href="# https://youtu.be/5Jv9XXSEFdk">
+                                    <text x="60" y="250" fontFamily="Arial" fontWeight="normal" fill="#E86A24" stroke="none" fontSize="3.3em">
+                                        Watch &#8594;
+                                    </text> 
+                                </a>
+                                    
+                                </g>
+                            </g>
+                            </g>
+                        </svg>
                     </div>
-                    
-                
-                <div className="w-full flex flex-col p-5 sm:w-1/2 lg:w-1/4">
-                    <div className="flex flex-col flex-none px-10 pt-10 pb-12 blue-delivers-square-img">
-                        <div className="flex-1">
-                            <h2 className="text-cta text-xl sm:text-xl lg:text-xl font-bold leading-snug">
-                                Omnichannel Communications Video
-                            </h2>
-                        </div>
-                        <a href="#  https://youtu.be/J-81QHjaTNo" 
-                            className="pt-2 inline-flex items-center py-3 text-kddiOrange font-normal text-2xl">
-                            Watch &#8594;
-                        </a>
+
+                    <div className="w-full flex flex-col p-5 sm:w-1/2 lg:w-1/4">
+                        <svg width="100%" height="100%" viewBox="0 0 599 299" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                            <g id="Page-1" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+                            <g id="Partner-Page" transform="translate(-336.000000, -1707.000000)" fill="#FFFFFF" stroke="#AED6F2" strokeWidth={8}>
+                                <g id="Modernize" transform="translate(340.000000, 1711.000000)">
+                                <polygon id="Rectangle-Copy-5" points="0 1.75873205e-13 591 0 591 217.603692 532.82783 291 0 291" />
+                                <text x="60" y="70" fontWeight="500" fill="#2B3175" stroke="none" fontSize="2.8em">
+                                    Omnichannel 
+                                </text>
+                                <text x="60" y="120" fontWeight="500" fill="#2B3175" stroke="none" fontSize="2.8em">
+                                    Communications
+                                </text>
+                                <text x="60" y="170" fontWeight="500" fill="#2B3175" stroke="none" fontSize="2.8em">
+                                    Video
+                                </text>
+                                <a href="# https://youtu.be/5Jv9XXSEFdk">
+                                    <text x="60" y="250" fontFamily="Arial" fontWeight="normal" fill="#E86A24" stroke="none" fontSize="3.3em">
+                                        Watch &#8594;
+                                    </text> 
+                                </a>
+                                    
+                                </g>
+                            </g>
+                            </g>
+                        </svg>
                     </div>
-                </div>
-                    {/* <div className="poligon">
-                     <img src="http://lorempixel.com/g/600/400/"/>
-                    </div> */}
+
                 </div>
             </div>
         </section>
